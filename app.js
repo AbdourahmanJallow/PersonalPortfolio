50e02d3ba70d9d776eb382e687a5288f9898fd65
Abdourahman Sheikh Jallow <dexmanjallow@gmail.com>
2022-11-14 12:58:38 +0000

    first commit

---
+++ app.js
@@ -0,0 +1,17 @@
+// const tablinks = document.querySelector('.tab-links');
+// const tabcontents = document.querySelector('.tab-contents');
+
+// tablinks.addEventListener('onclick',opentab);
+// tabcontents.addEventListener('onclick',opentab);
+
+// function opentab(){
+//   tablinks.forEach(function(tablink){
+//     if(tablink.classList.contains('active-link')){
+//       tablink.classList.remove("active-link");
+//     }
+//   });
+
+//   tabcontents.forEach(function (tabcontent) {
+//     tabcontent.classList.remove("active-tab");
+//   });
+// }
